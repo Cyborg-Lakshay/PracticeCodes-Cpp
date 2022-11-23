3fc4172ea9f1633738dc1f94208dcb480b5548dd
Cyborg-Lakshay <118838493+Cyborg-Lakshay@users.noreply.github.com>
2022-11-23 06:21:44 +0530

    Add files via upload

---
+++ Sum_of_natural no/Natural_no.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n ";
+    cin >> n;
+    
+    int Sum=(n*(n+1))/2;
+    cout << "Sum of the given natural number is " << Sum;
+    return 0;  
+}
