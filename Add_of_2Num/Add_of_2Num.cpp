90714c75d9a7319af039e4ba07419358a5f78231
Cyborg-Lakshay <118838493+Cyborg-Lakshay@users.noreply.github.com>
2022-11-23 06:20:48 +0530

    Add files via upload

---
+++ Add_of_2Num/Add_of_2Num.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<a;
+    // cin>>a>>b;
+    // c=a+b;
+    // cout<<"Sum of the two numbers are  "<<c;
+    // return 0;
+}
