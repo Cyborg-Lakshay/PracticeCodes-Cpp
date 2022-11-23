535dd85df4277d5fa4dc91eccc903448d6e67947
Cyborg-Lakshay <118838493+Cyborg-Lakshay@users.noreply.github.com>
2022-11-23 06:06:29 +0530

    Add files via upload

---
+++ AoT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float breadth,height;
+    cout<<"Breadth of triangle is ";
+    cin>>breadth;
+    cout<<"Height of triangle is ";
+    cin>>height;
+    
+    float Area=(breadth*height)/2;
+     
+    cout<<"Area of Triangle is "<<Area;
+    return 0;
+
+}
