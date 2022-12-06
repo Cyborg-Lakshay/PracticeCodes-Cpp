cb8faf124889f41792b320d673e9555cbadb1740
Lakshay Dhattarwal <118838493+Cyborg-Lakshay@users.noreply.github.com>
2022-12-06 23:47:33 +0530

    Add files via upload

---
+++ Function for even odd/even_odd_func.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+int oddEven ()
+{
+    int a;
+    cin>>a;
+
+    if(a%2==0)
+    {
+        cout<<"Even number";
+
+    }
+    else
+    {
+        cout<<"Odd number";
+    }
+    return 0;
+}   
+
+int main()
+{
+
+    oddEven();
+
+}
